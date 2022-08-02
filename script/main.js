18b4c0f0dbe879bcd7d705f505904ad33c8eb3c3
Willian Igor <willianigorprof@gmail.com>
2022-08-02 17:22:01 -0300

    add Header and Home

--- script/main.js
+++ script/main.js
@@ -1,4 +1,14 @@
 // DOM Loaded
 document.addEventListener('DOMContentLoaded', () => {
 
+    // MenuHandlle
+    document.querySelectorAll('.menuHandlle').forEach(element => {
+        element.addEventListener('click', () => {
+            document.querySelector('#menu').classList.toggle('show')
+            document.querySelector('#open-menu').classList.toggle('show')
+            document.querySelector('#open-menu').classList.toggle('hiden')
+            document.querySelector('#close-menu').classList.toggle('show')
+            document.querySelector('#close-menu').classList.toggle('hiden')
+        })
+    })
 })
